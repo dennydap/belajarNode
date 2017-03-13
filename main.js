656c43bf85cd63e16dc6392af9b24778d688760f
Denny Pradipta <denny.aditya.p@gmail.com>
2017-03-13 10:01:03 +0700

    Belajar konsep listener

--- main.js
+++ main.js
@@ -15,30 +15,77 @@
 // console.log("Hello, World!");
 // console.log("Coba sekarang buka http://localhost:8081");
 
-// Mengimport modul events
-var events = require('events');
+/////////////////////////////
 
-// Membuat objek eventEmitter
+// // Mengimport modul events
+// var events = require('events');
+
+// // Membuat objek eventEmitter
+// var eventEmitter = new events.EventEmitter();
+
+// // Membuat event handler
+// var connectHandler = function connected() {
+// 	// Munculkan pesan pada console
+// 	console.log('Koneksi sukses.');
+
+// 	// Emit event data_received
+// 	eventEmitter.emit('data_received');
+// }
+
+// // Menghubungkan connection event dengan handlernya
+// eventEmitter.on('connection', connectHandler);
+
+// // Menghubungkan event data_received dengan fungsi anonim
+// eventEmitter.on('data_received', function() {
+// 	console.log('Data sukses diterima.');
+// });
+
+// // Emit connection event
+// eventEmitter.emit('connection');
+
+// console.log("Selesai.");
+
+///////////////////////////
+
+//Deklarasi modul dan variable untuk EventEmitter
+var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
-// Membuat event handler
-var connectHandler = function connected() {
-	// Munculkan pesan pada console
-	console.log('Koneksi sukses.');
-
-	// Emit event data_received
-	eventEmitter.emit('data_received');
+// Listener 1
+var listener1 = function listener1() {
+	console.log('Listener1 aktif');
 }
 
-// Menghubungkan event connection dengan handlernya
-eventEmitter.on('connection', connectHandler);
+// Listener 2
+var listener2 = function listener2() {
+	console.log('Listener2 aktif');
+}
 
-// Menghubungkan event data_received dengan fungsi anonim
-eventEmitter.on('data_received', function() {
-	console.log('Data sukses diterima.');
-});
+// Menghubungkan connection event dengan fungsi listener1
+console.log('Menyalakan Listener1...');
+eventEmitter.addListener('connection', listener1);
 
-// Emit event connection
+// Menghubungkan connection event dengan fungsi listener2
+console.log('Menyalakan Listener2...');
+eventEmitter.on('connection', listener2);
+
+// Melakukan emit pada connection event
 eventEmitter.emit('connection');
 
-console.log("Selesai.");
+// Menghitung jumlah listeners
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log("Ada " + eventListeners + " Listener yang melakukan listening pada connection event.");
+
+// Melepas listener1 dari eventEmitter
+console.log('Mematikan Listener1...')
+eventEmitter.removeListener('connection', listener1);
+console.log('Listener1 sudah tidak melakukan listening lagi.');
+
+// Melakukan emit pada connection event
+eventEmitter.emit('connection');
+
+// Menghitung kembali jumlah listeners
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log("Ada " + eventListeners + " Listener yang melakukan listening pada connection event.");
+
+console.log("Selesai");
